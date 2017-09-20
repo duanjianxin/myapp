023901ca559293db5939ce38ca8caeb2f398df7a
段建新Home <719866109@qq.com>
2017-09-20 22:28:45 +0800

    完成轮播适配

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { CarouselComponent } from './home/carousel/carousel.component';
+import { MainComponent } from './home/main/main.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CarouselComponent } from './home/carousel/carousel.component';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    CarouselComponent
+    CarouselComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
